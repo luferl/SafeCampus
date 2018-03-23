c01ca5662ad287282dbee1b1ab5e3d9a7e4bf93d
Lufer <zxcmlmh@163.com>
2018-03-23 17:17:44 +0800

    试卷相关功能
    完成试卷的获取与展示
    下一步是试卷的保存，试卷配置功能的实现

---
+++ src/pcadmin/GetQuizlist.java
@@ -0,0 +1,92 @@
+package pcadmin;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetQuizlist
+ */
+@WebServlet("/pc/GetQuizlist")
+public class GetQuizlist extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetQuizlist() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Connection connection = null;
+		response.setContentType("application/json;charset=utf-8");
+		response.setCharacterEncoding("utf-8");
+		String json="[";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://127.0.0.1/safecampus";
+			connection = DriverManager.getConnection(url, "root", "123456");
+			String sql="SELECT * FROM quizes where isdeleted=0";
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			ResultSet re = preparedStatement.executeQuery();
+			int count=0;
+			while(re.next()){ 
+				String id=re.getString("ID");
+				String text=re.getString("name");
+				String starttime=re.getString("starttime");
+				String endtime=re.getString("endtime");
+				String time=re.getString("time");
+				String totalsc=re.getString("totalsc");
+				String passsc=re.getString("passsc");
+				String times=re.getString("times");
+				String issimulate=re.getString("issimulate");
+				if(count>0)
+					json=json+",";
+				json=json+"{\"id\":"+id+",\"text\":\""+text+"\",\"starttime\":\""+starttime+"\",\"endtime\":\""+endtime+"\",\"time\":\""+time+"\",\"totalsc\":"+totalsc+",\"passsc\":\""+passsc+"\",\"times\":\""+times+"\",\"issimulate\":\""+issimulate+"\"}";
+				count++;
+			 }
+			json=json+"]";
+			System.out.print(json);
+			response.getWriter().print(json);
+		}
+		catch(ClassNotFoundException e) {   
+			System.out.println("Sorry,can`t find the Driver!");   
+			e.printStackTrace();   
+		} 
+		catch(SQLException e) {
+			//数据库连接失败异常处理
+			e.printStackTrace();  
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally{
+			System.out.println("目录成功获取！！");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
