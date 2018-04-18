2c291481f2ad25d5d515d8ad5a1714e4745faa6a
Lufer <zxcmlmh@163.com>
2018-04-18 23:43:20 +0800

    自定义下载
    完成自定义下载，查询未参加考试人员名单在逻辑上有错误，放弃这个功能点
    分组统计开了个头，没想好sql怎么写

---
+++ src/pcadmin/CustomDownload.java
@@ -0,0 +1,140 @@
+package pcadmin;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+import com.sun.org.apache.regexp.internal.RE;
+
+import PublicClass.ExcelWriter;
+
+/**
+ * Servlet implementation class SaveDirect
+ */
+@WebServlet("/pc/CustomDownload")
+public class CustomDownload extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CustomDownload() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String id=request.getParameter("id");
+		String department=request.getParameter("department");
+		String role=request.getParameter("role");
+		String year=request.getParameter("year");
+		String gradestype=request.getParameter("gradestype");
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://127.0.0.1/safecampus";
+			connection = DriverManager.getConnection(url, "root", "123456");
+			String sql="";
+			sql="select passsc,name from quizes where ID="+id;
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			ResultSet rs=preparedStatement.executeQuery();
+			int passsc=0;
+			String quizname="";
+			if(rs.next())
+			{
+				passsc=rs.getInt("passsc");
+				quizname=rs.getString("name");
+			}
+			if(!role.equals("role"))
+				role="'"+role+"'";
+			if(!department.equals("department"))
+				department="'"+department+"'";
+			if(gradestype.equals("all"))
+			{
+				sql="select department,year,role,name,code,grades from(SELECT quizid,userid,max(grades) as grades FROM quiz_grades where quizid="+id+" group by userid ) as aa,users where aa.userid=users.ID and department="+department+" and role="+role+" and year="+year;
+			}
+			else
+				if(gradestype.equals("pass"))
+				{
+					sql="select department,year,role,name,code,grades from(SELECT quizid,userid,max(grades) as grades FROM quiz_grades where quizid="+id+" group by userid ) as aa,users where aa.userid=users.ID and department="+department+" and role="+role+" and year="+year+" and grades >="+passsc;
+				}
+			else
+				if(gradestype.equals("unpass"))
+				{
+					sql="select department,year,role,name,code,grades from(SELECT quizid,userid,max(grades) as grades FROM quiz_grades where quizid="+id+" group by userid ) as aa,users where aa.userid=users.ID and department="+department+" and role="+role+" and year="+year+" and grades <"+passsc;
+				}
+			/*
+				else
+				{
+					sql="select department,year,role,name,code,grades from(SELECT quizid,userid,max(grades) as grades FROM quiz_grades where quizid="+id+" group by userid ) as aa,users where aa.userid=users.ID and department='"+department+"' and role='"+role+"' and year="+year+" and grades >="+passsc;
+				}
+				*/
+			//sql="select department,year,role,name,code,grades from(SELECT quizid,userid,max(grades) as grades FROM quiz_grades where quizid="+id+" group by userid ) as aa,users where aa.userid=users.ID";
+			System.out.println(sql);
+			preparedStatement = connection.prepareStatement(sql);
+			rs=preparedStatement.executeQuery();
+			String realPath = getServletContext().getRealPath("pc/Download");
+			ExcelWriter ew=new ExcelWriter();
+			ew.writeexcel(rs, passsc,1,realPath);
+			String filename="自定义下载-"+quizname+".xls";
+			String userAgent = request.getHeader("User-Agent");  
+			  if (userAgent.contains("MSIE") || userAgent.contains("Trident")) {  
+				  filename = java.net.URLEncoder.encode(filename, "UTF-8");  
+	            } else {  
+	                // 非IE浏览器的处理：  
+	            	filename = new String(filename.getBytes("UTF-8"), "ISO-8859-1");  
+	            }   
+			String contentType = this.getServletContext().getMimeType("Grades.xls");
+			response.setHeader("Content-Type",contentType);
+			response.setHeader("content-disposition","attachment;filename="+filename);
+            // 读取要下载的文件，保存到文件输入流
+            FileInputStream in = new FileInputStream(realPath + "/Grades.xls");
+            // 创建输出流
+            ServletOutputStream out = response.getOutputStream();
+            IOUtils.copy(in,out);
+            in.close();
+		}
+		catch(ClassNotFoundException e) {   
+			System.out.println("Sorry,can`t find the Driver!");   
+			e.printStackTrace();   
+		} 
+		catch(SQLException e) {
+			//数据库连接失败异常处理
+			e.printStackTrace();  
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally{
+			System.out.println("Create Directories finished");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request,response);
+	}
+
+}
